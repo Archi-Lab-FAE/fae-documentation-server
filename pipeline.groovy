cae6a2657b630353a18aa4e86590422d72b8de80
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-09-09 12:29:04 +0200

    add docker-compose files + deplo step in jenkinsfile

--- pipeline.groovy
+++ pipeline.groovy
@@ -57,4 +57,13 @@ node {
             customImage.push('latest')
         }
     }
+
+    stage('Deploy') {
+        docker.withServer('tcp://10.10.10.61:2376', 'fae-ws2019-certs') {
+            docker.withRegistry('https://docker.nexus.archi-lab.io', 'archilab-nexus-jenkins') {
+                sh "env TAG=${env.BUILD_ID} docker stack deploy --with-registry-auth \
+                    -c src/main/docker/docker-compose-prod.yml"
+            }
+        }
+    }
 }
