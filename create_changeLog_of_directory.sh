75a6221ebf15e5b397d4ec3ae129ef11fe759994
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-09-12 14:48:42 +0200

    update script, fix bug with new line

--- create_changeLog_of_directory.sh
+++ create_changeLog_of_directory.sh
@@ -8,6 +8,7 @@ do
     *.md | *.mdown | *.mkdn | *.mkd | *.markdown)
 
       printf "### ChangeLog  \n" >>$entry
+      printf "  \n" >> $entry
       printf "| Commit | Commit-Message | Datum | Nutzer |  \n|-------|-------|--------|--------|  \n">>$entry
       git log --pretty=format:"| %h | %s | %ad | %an |  " --date=short "$entry" >> $entry
       ;;
