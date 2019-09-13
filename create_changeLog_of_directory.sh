81324f93c47bd5a4454e77fc451ae1b83f1a951b
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-09-13 15:33:09 +0200

    update script

--- create_changeLog_of_directory.sh
+++ create_changeLog_of_directory.sh
@@ -7,10 +7,10 @@ do
   case "$entry" in
     *.md | *.mdown | *.mkdn | *.mkd | *.markdown)
 
-      printf "### Changelog  \n" >>$entry
-      printf "  \n" >> $entry
-      printf "| Commit | Commit-Message | Date | Author |  \n|-------|-------|--------|--------|  \n">>$entry
-      git log --pretty=format:"| %h | %s | %ad | %an |  " --date=short "$entry" >> $entry
+      printf "### Changelog  \n" >>"$entry"
+      printf "  \n" >> "$entry"
+      printf "| Commit | Commit-Message | Date | Author |  \n|-------|-------|--------|--------|  \n">>"$entry"
+      git log --pretty=format:"| %h | %s | %ad | %an |  " --date=short "$entry" >> "$entry"
       ;;
     *)
       echo "No MD: $entry"
