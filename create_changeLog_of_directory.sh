52bf7b26b85c0c00c996b71fecbd06b184ce5dad
JannIntveen <39293485+JannIntveen@users.noreply.github.com>
2019-10-10 10:28:53 +0200

    add whitespace before changelog

--- create_changeLog_of_directory.sh
+++ create_changeLog_of_directory.sh
@@ -7,7 +7,7 @@ do
   case "$entry" in
     *.md | *.mdown | *.mkdn | *.mkd | *.markdown)
 
-      printf "### Changelog  \n" >>"$entry"
+      printf "\n### Changelog  \n" >>"$entry"
       printf "  \n" >> "$entry"
       printf "| Commit | Commit-Message | Date | Author |  \n|-------|-------|--------|--------|  \n">>"$entry"
       git log --pretty=format:"| %h | %s | %ad | %an |  " --date=short "$entry" >> "$entry"
